774ca407bfb73c1b45976889425d9371c2794d82
francisco-1998 <francmarinc@gmail.com>
2021-01-30 10:01:40 -0500

    Segmentoel codigo para hacerlo reutilizable e implementacion del forms-module para el componente de buscar

--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -4,6 +4,8 @@ import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -15,7 +17,9 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     VerPaisComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    FormsModule
   ],
   exports:[
     PorCapitalComponent,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SearchComponent } from './search/search.component';
+import { TableInfoComponent } from './table-info/table-info.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
-  declarations: [SidebarComponent],
+  declarations: [SidebarComponent, SearchComponent, TableInfoComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   exports:[
-    SidebarComponent
+    SidebarComponent,
+    SearchComponent,
+    TableInfoComponent
+
   ]
 })
 export class SharedModule { }
