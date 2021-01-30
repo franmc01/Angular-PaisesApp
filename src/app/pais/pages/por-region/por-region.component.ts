c8a401c61744f00d6f01d363a02ebb1a4bac8502
francisco-1998 <francmarinc@gmail.com>
2021-01-30 15:47:38 -0500

    Entendiendo ngClass, class y [class.]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorRegionComponent implements OnInit {
 
+  regiones:string[] = ['africa', 'americas', 'asia', 'europe', 'oceania']
+  regionActiva:string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  consultarRegion(region:string){
+    this.regionActiva = region;
   }
 
 }
